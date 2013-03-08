55d6db4013930bd3731425ef1d5334cb81c9482c
crynobone <crynobone@gmail.com>
2013-03-08 16:24:55 +0800

    Update navigation to use Orchestra\Decorator::navbar()
    Signed-off-by: crynobone <crynobone@gmail.com>

--- views/widgets/menu.blade.php
+++ views/widgets/menu.blade.php
@@ -1,23 +1,21 @@
-<div class="navbar">
-  <div class="navbar-inner">
+@section('melody::primary_menu')
+<ul class="nav">
+	<li class="{{ URI::is('*/manages/melody.themes/frontend*') ? 'active' : '' }}">
+		{{ HTML::link(handles('orchestra::manages/melody.themes/frontend'), 'Frontend') }}
+	</li>
+	<li class="{{ URI::is('*/manages/melody.themes/backend*') ? 'active' : '' }}">
+		{{ HTML::link(handles('orchestra::manages/melody.themes/backend'), 'Backend') }}
+	</li>
+</ul>
+@endsection
 
-  		<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target="#cellonav">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-		
-		{{ HTML::link(handles('orchestra::manages/melody.themes'), 'Themes Manager', array('class' => 'brand')) }}
+<?php
 
-		<div id="cellonav" class="collapse nav-collapse">
-		  	<ul class="nav">
-				<li class="{{ URI::is('*/manages/melody.themes/frontend*') ? 'active' : '' }}">
-					{{ HTML::link(handles('orchestra::manages/melody.themes/frontend'), 'Frontend') }}
-				</li>
-				<li class="{{ URI::is('*/manages/melody.themes/backend*') ? 'active' : '' }}">
-					{{ HTML::link(handles('orchestra::manages/melody.themes/backend'), 'Backend') }}
-				</li>
-			</ul>
-		</div>
-	</div>
-</div>
+$navbar = new Orchestra\Fluent(array(
+	'id'             => 'melody',
+	'title'          => 'Theme Manager',
+	'url'            => handles('orchestra::manages/melody.themes'),
+	'primary_menu'   => Laravel\Section::yield('melody::primary_menu'),
+)); ?>
+
+{{ Orchestra\Decorator::navbar($navbar) }}
